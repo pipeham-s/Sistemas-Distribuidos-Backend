766b9c7822207fb314e8210aa1b82e8ef92385c3
pipeham-s <pipeham03@gmail.com>
2024-09-05 12:12:55 -0300

    Initial commit

---
+++ src/main/java/com/example/backend_sistemas_distribuidos/BackendSistemasDistribuidosApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_sistemas_distribuidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSistemasDistribuidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSistemasDistribuidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_sistemas_distribuidos/BackendSistemasDistribuidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_sistemas_distribuidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSistemasDistribuidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
